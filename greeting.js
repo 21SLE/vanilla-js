a05db128477891bb79057ec4128c8fe8afe1f5e2
21SLE <hssarah@mail.hongik.ac.kr>
2020-09-09 00:03:47 +0900

    Saving the User Name

--- greeting.js
+++ greeting.js
@@ -1,14 +1,42 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser";
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
 
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
+    // 사용자가 자기이름 입력하면 그게 로컬 스토리지에 저장될거아냐 
+    // 그걸 가져오기.
+    
     if(currentUser === null) {
-
-    }else {
-        
+        askForName();
+    } else {
+        paintGreeting(currentUser);
     }
 }
 
