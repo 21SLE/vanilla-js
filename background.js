dcdc00d75d5117691d0332de2c8f456f159917f2
Hansle Lee <hssarah13@gmail.com>
2020-09-12 20:57:10 +0900

    image background

---
+++ background.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMAGE_NUMBER = 5; //갖고있는 이미지 개수
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`
+    // +1하는 이유는 랜덤넘버가 0이될수도 있기때문 
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * 3);
+    return number;
+
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+};
+
+init();
